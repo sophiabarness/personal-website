7e3dda0ae204da8e1da4aeb7df553c30403bcaaf
Sophia <animalsnow750@yahoo.com>
2026-03-02 01:51:48 -0800

    clean up dead code and placeholder posts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,17 +1,10 @@
-import { getBlogPosts } from 'app/portfolio/utils'
-
 export const baseUrl = 'https://sonya.dev'
 
 export default async function sitemap() {
-    let blogs = getBlogPosts().map((post) => ({
-        url: `${baseUrl}/portfolio/${post.slug}`,
-        lastModified: post.metadata.publishedAt,
-    }))
-
-    let routes = ['', '/blog'].map((route) => ({
+    let routes = ['', '/agent', '/portfolio'].map((route) => ({
         url: `${baseUrl}${route}`,
         lastModified: new Date().toISOString().split('T')[0],
     }))
 
-    return [...routes, ...blogs]
+    return [...routes]
 }
